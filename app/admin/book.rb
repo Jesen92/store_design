1a6106129ac8e3bcefa2c3f7f654b3abec0f317f
Hrvoje <hjesenovic@gmail.com>
2015-05-28 22:01:58 +0200

    dodan admin

---
+++ app/admin/book.rb
@@ -0,0 +1,48 @@
+ActiveAdmin.register Book do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+index do
+	selectable_column
+	column :id
+	column :title
+	column :author
+	column :year
+	column :price
+	column :format
+	column :pages
+	column :created_at
+	column :updated_at
+  actions 
+end
+
+#form do |f|
+ #   f.inputs "Details" do
+  #    f.input :category
+   #   f.input :author
+    #  f.input :publisher
+     # f.input :title
+      #f.input :isbn
+     # f.input :year
+    #  f.input :price
+    #  f.input :buy
+    #  f.input :excerpt
+    #  f.input :format
+    #  f.input :pages
+    #  f.input :coverpath
+    #  f.actions
+   # end
+ # end
+
+end
